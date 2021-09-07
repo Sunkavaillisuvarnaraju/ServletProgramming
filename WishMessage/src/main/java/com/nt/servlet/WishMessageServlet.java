eca4d9a7591a5af3516e46da2006dc27daefca75
SLV SLO <SLV SLO@DESKTOP-A9D5BF7>
2021-09-07 13:26:10 +0530

    Wish Message Total Application

---
+++ WishMessage/src/main/java/com/nt/servlet/WishMessageServlet.java
@@ -0,0 +1,19 @@
+package com.nt.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class WishMessageServlet extends HttpServlet{
+	
+	public void service(HttpServletRequest req,HttpServletResponse res) throws IOException {
+		//Create PrintWriter Object
+		PrintWriter pw=res.getWriter();
+		
+		
+	}
+
+}
